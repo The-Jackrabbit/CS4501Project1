794690b18559d52a06e41622582f961802d1b53e
The-Jackrabbit <lsm5fm@virginia.edu>
2018-01-29 20:01:27 -0500

    Fix for settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'mysql.connector.django',
+         'ENGINE': 'django.db.backends.mysql',
          'NAME': 'cs4501',
          'USER': 'www',
          'PASSWORD': '$3cureUS',
